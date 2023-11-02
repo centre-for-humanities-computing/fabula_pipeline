1404d9b5863303c0cf67b9ef1555c76987d344e1
miscodisco <mia.jacobsen@live.dk>
2023-11-02 14:39:09 +0100

    made src folder with utils script and .sh file for running pipe

---
+++ src/pipeline.py
@@ -0,0 +1,208 @@
+"""
+ this pipeline assumes that books are saved as txt-files within a folder :)
+ 
+TO DO:
+[ ] implement afinn for danish
+[ ] add ucloud / local argument
+[ ] add vader / syuzhet / avg of both arugment 
+[ ] setup try/except for roget
+[X] implement language argument
+[X] implement danish pipeline 
+    needs to be tested tho (6-10-2023)
+    ran on all books on ucloud(2-11-2023)
+[ ] fix pandas SettingWithCopyWarning
+[ ] the roget categories don't seem right or ?? 
+[X] make utils script? 
+
+"""
+import argparse
+import json
+from pathlib import Path
+
+from afinn import Afinn
+from lexical_diversity import lex_div as ld
+import neurokit2 as nk
+from nltk.tokenize import sent_tokenize
+import nltk
+import spacy
+
+from utils import *
+
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="FABULA-NET pipeline")
+    parser.add_argument("--in_dir", type=str)
+    parser.add_argument("--out_dir", type=str, default="output/")
+    parser.add_argument("-lang", type=str, default="english")
+    parser.add_argument("--ucloud", type=bool, default=True)
+    parser.add_argument("--sentiment_method", "-sent", type=str, default="vader")
+
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    in_dir = Path(args.in_dir)
+    out_dir = Path(args.out_dir)
+
+    nltk.download("punkt")
+
+    if args.lang == "english":
+        nltk.download("vader_lexicon")
+
+        try:
+            nlp = spacy.load("en_core_web_sm")
+        except OSError as e:
+            raise OSError(
+                "en_core_web_sm not downloaded, run python3 -m spacy download en_core_web_sm"
+            ) from e
+
+    elif args.lang == "danish":
+        try:
+            nlp = spacy.load("da_core_news_sm")
+
+        except OSError as e:
+            raise OSError(
+                "da_core_news_sm not downloaded, run python3 -m spacy download da_core_news_sm"
+            ) from e
+
+    nlp.max_length = 3500000
+
+    master_dict = {}
+
+    print("starting loop")
+    for filename in Path(in_dir).glob("*.txt"):
+        temp = {}
+
+        text = extract_text(filename)
+
+        sents = sent_tokenize(text, language=args.lang)
+        words = word_tokenize(text, language=args.lang)
+
+        # spacy
+        spacy_attributes = []
+        for token in nlp(text):
+            token_attributes = get_spacy_attributes(token)
+            spacy_attributes.append(token_attributes)
+
+        spacy_df = create_spacy_df(spacy_attributes)
+
+        save_spacy_df(spacy_df, filename, out_dir)
+
+        # stylometrics
+        # for words
+        temp["word_count"] = len(words)
+        temp["average_wordlen"] = avg_wordlen(words)
+        temp["msttr"] = ld.msttr(words, window_length=100)
+
+        # for sentences
+        if len(sents) < 1502:
+            print(f"\n{filename.name}")
+            print("text not long enough for stylometrics\n")
+            pass
+        else:
+            temp["average_sentlen"] = avg_sentlen(sents)
+            (
+                temp["gzipr"],
+                temp["bzipr"],
+            ) = compressrat(sents)
+
+        # bigram entropy
+        try:
+            temp["bigram_entropy"], temp["word_entropy"] = text_entropy(
+                text, language=args.lang, base=2, asprob=False
+            )
+        except:
+            print(f"\n{filename.name}")
+            print("error in bigram entropy\n")
+            pass
+
+        # doing stuff that only works in english
+        if args.lang == "english":
+            # basic sentiment features
+
+            arc = get_sentarc(sents)
+
+            if len(arc) < 60:
+                print(f"\n{filename.name}")
+                print("arc not long enough for basic sentiment features\n")
+                pass
+            else:
+                (
+                    temp["mean_sentiment"],
+                    temp["std_sentiment"],
+                    temp["mean_sentiment_per_segment"],
+                    temp["mean_sentiment_first_ten_percent"],
+                    temp["mean_sentiment_last_ten_percent"],
+                    temp["difference_lastten_therest"],
+                ) = get_basic_sentarc_features(arc)
+
+            # approximate entropy
+            try:
+                temp["approximate_entropy"] = nk.entropy_approximate(
+                    arc, dimension=2, tolerance="sd"
+                )
+            except:
+                print(f"\n{filename.name}")
+                print("error with approximate entropy\n")
+                pass
+
+            # hurst
+            try:
+                temp["hurst"] = get_hurst(arc)
+            except:
+                print(f"\n{filename.name}")
+                print("error with hurst\n")
+                pass
+
+                # readability
+            try:
+                (
+                    temp["flesch_grade"],
+                    temp["flesch_ease"],
+                    temp["smog"],
+                    temp["ari"],
+                    temp["dale_chall_new"],
+                ) = text_readability(text)
+
+            except:
+                print(f"\n{filename.name}")
+                print("error in readability\n")
+                pass
+
+            # roget
+            all_roget_categories = roget.list_all_categories()
+
+            roget_df = filter_spacy_df(spacy_df)
+
+            temp["roget_n_tokens"] = len(spacy_df)
+            temp["roget_n_tokens_filtered"] = len(roget_df)
+
+            token_categories = get_token_categories(roget_df)
+            doc_categories = re.findall(r"(rog\d{3} \w*)", token_categories)
+
+            for roget_cat in all_roget_categories:
+                temp[roget_cat] = doc_categories.count(roget_cat)
+
+            temp["roget_n_cats"] = len(doc_categories)
+
+            # save arc
+            temp["arc"] = arc
+
+        # saving it all
+        master_dict[filename.stem] = temp
+
+    print("finished loop")
+
+    Path(out_dir).mkdir(exist_ok=True)
+
+    print("saving file")
+    with open(out_dir.joinpath("books_features.json"), "w") as f:
+        json.dump(master_dict, f)
+    print("done :-)")
+
+
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,18 @@
-"""
- this pipeline assumes that books are saved as txt-files within a folder :)
- 
-TO DO:
-[ ] setup try/except for roget
-[X] implement language argument
-[X] implement danish pipeline 
-    needs to be tested tho (6-10-2023)
-[ ] fix pandas SettingWithCopyWarning
-[ ] the roget categories don't seem right or ?? 
-[ ] make utils script? 
-
-"""
-import argparse
 import bz2
 from collections import Counter
 import gzip
-import json
 from math import log
 import re
 from pathlib import Path
 
-from afinn import Afinn
-from lexical_diversity import lex_div as ld
-import neurokit2 as nk
-import nltk
 from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import numpy as np
 import pandas as pd
-import spacy
 import textstat
 
 import saffine.multi_detrending as md
 import roget.roget as roget
-
-
-def create_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(prog="FABULA-NET pipeline")
-    parser.add_argument("--in_dir", type=str)
-    parser.add_argument("--out_dir", type=str, default="output/")
-    parser.add_argument("--language", "-lang", type=str, default="english")
-
-    return parser
 
 
 def extract_text(filename: str) -> str:
@@ -202,7 +172,7 @@ def text_entropy(text: str, language: str, base=2, asprob=True, clean=True):
     # Loop through each word in the cleaned text and calculate the probability of each bigram
     for i, word in enumerate(words):
         if i == 0:
-            word_transform_prob[word]+=1
+            word_transform_prob[word] += 1
 
             # very first word gets assigned as first pre
             pre = word
@@ -214,7 +184,7 @@ def text_entropy(text: str, language: str, base=2, asprob=True, clean=True):
 
     # return transformation probability if asprob is set to true
     if asprob:
-        return transform_prob
+        return word_transform_prob, bigram_transform_prob
     # if not, calculate the entropy and return that
     if not asprob:
         log_n = log(total_len, base)
@@ -308,172 +278,3 @@ def get_token_categories(df: pd.DataFrame) -> str:
     ).to_string()
 
     return token_categories
-
-
-def main():
-    parser = create_parser()
-    args = parser.parse_args()
-
-    in_dir = Path(args.in_dir)
-    out_dir = Path(args.out_dir)
-
-    nltk.download("punkt")
-
-    if args.lang == "english":
-        nltk.download("vader_lexicon")
-
-        try:
-            nlp = spacy.load("en_core_web_sm")
-        except OSError as e:
-            raise OSError(
-                "en_core_web_sm not downloaded, run python3 -m spacy download en_core_web_sm"
-            ) from e
-
-    elif args.lang == "danish":
-        try:
-            nlp = spacy.load("da_core_news_sm")
-
-        except OSError as e:
-            raise OSError(
-                "da_core_news_sm not downloaded, run python3 -m spacy download da_core_news_sm"
-            ) from e
-
-    nlp.max_length = 3500000
-
-    master_dict = {}
-
-    print("starting loop")
-    for filename in Path(in_dir).glob("*.txt"):
-        temp = {}
-
-        text = extract_text(filename)
-
-        sents = sent_tokenize(text, language=args.lang)
-        words = word_tokenize(text, language=args.lang)
-
-        # spacy
-        spacy_attributes = []
-        for token in nlp(text):
-            token_attributes = get_spacy_attributes(token)
-            spacy_attributes.append(token_attributes)
-
-        spacy_df = create_spacy_df(spacy_attributes)
-
-        save_spacy_df(spacy_df, filename, out_dir)
-
-        # stylometrics
-        # for words
-        temp["word_count"] = len(words)
-        temp["average_wordlen"] = avg_wordlen(words)
-        temp["msttr"] = ld.msttr(words, window_length=100)
-
-        # for sentences
-        if len(sents) < 1502:
-            print(f"\n{filename.name}")
-            print("text not long enough for stylometrics\n")
-            pass
-        else:
-            temp["average_sentlen"] = avg_sentlen(sents)
-            (
-                temp["gzipr"],
-                temp["bzipr"],
-            ) = compressrat(sents)
-
-        # bigram entropy
-        try:
-            temp["bigram_entropy"], temp["word_entropy"] = text_entropy(
-                text, language=args.lang, base=2, asprob=False
-            )
-        except:
-            print(f"\n{filename.name}")
-            print("error in bigram entropy\n")
-            pass
-
-
-        # doing stuff that only works in english
-        if args.lang == "english":
-            # basic sentiment features
-
-            arc = get_sentarc(sents)
-
-            if len(arc) < 60:
-                print(f"\n{filename.name}")
-                print("arc not long enough for basic sentiment features\n")
-                pass
-            else:
-                (
-                    temp["mean_sentiment"],
-                    temp["std_sentiment"],
-                    temp["mean_sentiment_per_segment"],
-                    temp["mean_sentiment_first_ten_percent"],
-                    temp["mean_sentiment_last_ten_percent"],
-                    temp["difference_lastten_therest"],
-                ) = get_basic_sentarc_features(arc)
-
-            # approximate entropy
-            try:
-                temp["approximate_entropy"] = nk.entropy_approximate(
-                    arc, dimension=2, tolerance="sd"
-                )
-            except:
-                print(f"\n{filename.name}")
-                print("error with approximate entropy\n")
-                pass
-
-            # hurst
-            try:
-                temp["hurst"] = get_hurst(arc)
-            except:
-                print(f"\n{filename.name}")
-                print("error with hurst\n")
-                pass
-
-                # readability
-            try:
-                (
-                    temp["flesch_grade"],
-                    temp["flesch_ease"],
-                    temp["smog"],
-                    temp["ari"],
-                    temp["dale_chall_new"],
-                ) = text_readability(text)
-
-            except:
-                print(f"\n{filename.name}")
-                print("error in readability\n")
-                pass
-
-            # roget
-            all_roget_categories = roget.list_all_categories()
-
-            roget_df = filter_spacy_df(spacy_df)
-
-            temp["roget_n_tokens"] = len(spacy_df)
-            temp["roget_n_tokens_filtered"] = len(roget_df)
-
-            token_categories = get_token_categories(roget_df)
-            doc_categories = re.findall(r"(rog\d{3} \w*)", token_categories)
-
-            for roget_cat in all_roget_categories:
-                temp[roget_cat] = doc_categories.count(roget_cat)
-
-            temp["roget_n_cats"] = len(doc_categories)
-
-            # save arc
-            temp["arc"] = arc
-
-        # saving it all
-        master_dict[filename.stem] = temp
-
-    print("finished loop")
-
-    Path(out_dir).mkdir(exist_ok=True)
-
-    print("saving file")
-    with open(out_dir.joinpath("books_features.json"), "w") as f:
-        json.dump(master_dict, f)
-    print("done :-)")
-
-
-if __name__ == "__main__":
-    main()
